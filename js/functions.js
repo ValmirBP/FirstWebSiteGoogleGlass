9d4b7128fc77d89d185ba5cf2829703120357ad5
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2023-12-23 16:46:05 -0500

    Form updated and working

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,9 @@
 function changePic(foto) {
     document.getElementById("IconId").src = foto
 }
+
+function calc_total() {
+    var qtd = parseInt(document.getElementById('cQntd').value);
+    var valor = qtd * 1500 * 6;
+    document.getElementById('cTot').value = valor;
+}
